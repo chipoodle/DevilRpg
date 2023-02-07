e67e5e2455a005ee8d9dd83d21d7b1d0e39283a8
Chipoodle <ccsh23@yahoo.com.mx>
2023-02-07 15:06:40 -0600

    Implemented soulvine block. Implemented werewolf vitality skill. changed icons.

---
+++ src/main/java/com/chipoodle/devilrpg/skillsystem/skillinstance/PlayerPassiveWerewolfVitality.java
@@ -0,0 +1,113 @@
+package com.chipoodle.devilrpg.skillsystem.skillinstance;
+
+import com.chipoodle.devilrpg.DevilRpg;
+import com.chipoodle.devilrpg.capability.IGenericCapability;
+import com.chipoodle.devilrpg.capability.auxiliar.IBaseAuxiliarCapability;
+import com.chipoodle.devilrpg.capability.auxiliar.PlayerAuxiliarCapabilityProvider;
+import com.chipoodle.devilrpg.capability.skill.IBasePlayerSkillCapability;
+import com.chipoodle.devilrpg.capability.skill.PlayerSkillCapability;
+import com.chipoodle.devilrpg.skillsystem.ISkillContainer;
+import com.chipoodle.devilrpg.util.SkillEnum;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class PlayerPassiveWerewolfVitality extends AbstractPlayerPassive implements ISkillContainer, ICapabilityAttributeModifier {
+    public static final String ATTRIBUTE_MODIFIER_UNIQUE_NAME = SkillEnum.WEREWOLF_VITALITY.name() + "_" + "ADDITION";
+    public static final double HEALTH_FACTOR = 1.00D;
+    AttributeModifier hitAttributeModifier;
+    private final IBasePlayerSkillCapability parentCapability;
+    private PlayerEntity playerIn;
+
+    public PlayerPassiveWerewolfVitality(PlayerSkillCapability parentCapability) {
+        this.parentCapability = parentCapability;
+        DevilRpg.LOGGER.info("----------------------->CONSTRUCTOR PlayerPassiveWerewolfVitality. Parent capability: {}", parentCapability);
+    }
+
+    /**
+     * *
+     *
+     * @param worldIn
+     * @param playerIn
+     * @param parameters Server side called
+     */
+    @Override
+    public void execute(World worldIn, PlayerEntity playerIn, HashMap<String, String> parameters) {
+        if (!worldIn.isClientSide) {
+
+            if (this.playerIn == null) {
+                this.playerIn = playerIn;
+            }
+            initializeAttributes(playerIn);
+
+            IBaseAuxiliarCapability auxiliary = IGenericCapability.getUnwrappedPlayerCapability(playerIn, PlayerAuxiliarCapabilityProvider.AUX_CAP);
+            if (auxiliary.isWerewolfTransformation()) {
+                add();
+            } else {
+                remove();
+            }
+            double maxHealth = playerIn.getAttributeValue(Attributes.MAX_HEALTH);
+            DevilRpg.LOGGER.info("max health {}", maxHealth);
+        }
+    }
+
+    private void initializeAttributes(PlayerEntity playerIn) {
+        if (hitAttributeModifier == null ||
+                hitAttributeModifier.getAmount() != Double.valueOf(parentCapability.getSkillsPoints().get(SkillEnum.WEREWOLF_VITALITY)) * HEALTH_FACTOR) {
+            removeCurrentWerwolfVitalityModifiers();
+            hitAttributeModifier = createNewAttributeModifiers();
+            HashMap<String, UUID> capAttModifiersHashMap = parentCapability.getAttributeModifiers();
+            addAttributeToCapability(capAttModifiersHashMap, Attributes.MAX_HEALTH, hitAttributeModifier.getId());
+            parentCapability.setAttributeModifiers(capAttModifiersHashMap, playerIn);
+            DevilRpg.LOGGER.info("----------------------->Add {}", hitAttributeModifier.getId());
+        }
+    }
+
+    public SkillEnum getSkillEnum() {
+        return SkillEnum.WEREWOLF_VITALITY;
+    }
+
+    private AttributeModifier createNewAttributeModifiers() {
+        AttributeModifier newAttributeModifier = createNewAttributeModifier(
+                ATTRIBUTE_MODIFIER_UNIQUE_NAME,
+                Double.valueOf(parentCapability.getSkillsPoints().get(SkillEnum.WEREWOLF_VITALITY)) * HEALTH_FACTOR
+                , AttributeModifier.Operation.ADDITION);
+        //DevilRpg.LOGGER.info("||----------------------->createNewAttributeModifiers SKIN_ARMOR: {}", parentCapability.getSkillsPoints().get(SkillEnum.SKIN_ARMOR));
+        DevilRpg.LOGGER.info("----------------------->createNewAttributeModifiers(): {}", newAttributeModifier);
+        return newAttributeModifier;
+    }
+
+    private void removeCurrentWerwolfVitalityModifiers() {
+        //HashMap<String, UUID> attributeModifiers = parentCapability.getAttributeModifiers();
+        removeCurrentModifierFromPlayer(playerIn, hitAttributeModifier, Attributes.MAX_HEALTH);
+        //UUID uuid = removeAttributeFromCapability(attributeModifiers, Attributes.MAX_HEALTH);
+        //parentCapability.setAttributeModifiers(attributeModifiers, playerIn);
+        //DevilRpg.LOGGER.info("----------------------->Remove {}",uuid);
+         if (playerIn.getHealth() > playerIn.getMaxHealth())
+            playerIn.setHealth(playerIn.getMaxHealth());
+        DevilRpg.LOGGER.info("----------------------->removeCurrentModifiers(): {}", hitAttributeModifier);
+    }
+
+    private void addCurrentModifiers() {
+        //HashMap<String, UUID> attributeModifiers = parentCapability.getAttributeModifiers();
+        //addAttributeToCapability(attributeModifiers, Attributes.ARMOR, skinArmorAttributeModifier.getId());
+        addCurrentModifierTransiently(playerIn, Attributes.MAX_HEALTH, hitAttributeModifier);
+        //parentCapability.setAttributeModifiers(attributeModifiers, playerIn);
+        //DevilRpg.LOGGER.info("----------------------->Add {}",skinArmorAttributeModifier.getId());
+        DevilRpg.LOGGER.info("----------------------->addCurrentModifierTransiently(): {}", hitAttributeModifier);
+    }
+
+    public void add() {
+        //removeCurrentModifiers();
+        addCurrentModifiers();
+
+    }
+
+    public void remove() {
+        removeCurrentWerwolfVitalityModifiers();
+    }
+}
---
+++ src/main/java/com/chipoodle/devilrpg/skillsystem/skillinstance/SkillSoulVine.java
@@ -0,0 +1,79 @@
+package com.chipoodle.devilrpg.skillsystem.skillinstance;
+
+import com.chipoodle.devilrpg.DevilRpg;
+import com.chipoodle.devilrpg.block.SoulVineBlock;
+import com.chipoodle.devilrpg.capability.player_minion.IBaseMinionCapability;
+import com.chipoodle.devilrpg.capability.player_minion.PlayerMinionCapabilityProvider;
+import com.chipoodle.devilrpg.capability.skill.PlayerSkillCapability;
+import com.chipoodle.devilrpg.config.DevilRpgConfig;
+import com.chipoodle.devilrpg.entity.SoulWolfEntity;
+import com.chipoodle.devilrpg.init.ModBlocks;
+import com.chipoodle.devilrpg.init.ModEntityTypes;
+import com.chipoodle.devilrpg.skillsystem.ISkillContainer;
+import com.chipoodle.devilrpg.util.SkillEnum;
+import com.chipoodle.devilrpg.util.TargetUtils;
+import net.minecraft.block.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.LazyOptional;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class SkillSoulVine implements ISkillContainer {
+    private final PlayerSkillCapability parentCapability;
+
+    public SkillSoulVine(PlayerSkillCapability parentCapability) {
+        this.parentCapability = parentCapability;
+    }
+
+    @Override
+    public SkillEnum getSkillEnum() {
+        return SkillEnum.SOULVINE;
+    }
+
+    @Override
+    public void execute(World worldIn, PlayerEntity playerIn, HashMap<String, String> parameters) {
+        if (!worldIn.isClientSide) {
+            Random rand = new Random();
+            worldIn.playSound(null, playerIn.getX(), playerIn.getY(), playerIn.getZ(),
+                    SoundEvents.CHICKEN_EGG, SoundCategory.NEUTRAL, 0.5F,
+                    0.4F / (rand.nextFloat() * 0.4F + 0.8F));
+            setVine(worldIn, playerIn);
+        }
+    }
+
+    private void setVine(World worldIn, PlayerEntity playerIn) {
+        BlockPos blockPos = playerIn.blockPosition();
+        Block block = ModBlocks.SOUL_VINE_BLOCK.get();
+        BlockState blockState = worldIn.getBlockState(blockPos);
+
+        Vector3d playerLookVector = playerIn.getLookAngle();
+        Direction nearest = Direction.getNearest(playerLookVector.x, 0, playerLookVector.z);
+
+        DevilRpg.LOGGER.info("-------->Direction: " + nearest);
+
+        if (blockState.getBlock().equals(Blocks.AIR))
+            worldIn
+                    .setBlock(
+                            blockPos,
+                            //block.defaultBlockState().setValue(SoulVineBlock.getPropertyForFace(playerViewDirection.), Boolean.valueOf(true)),
+                            block.defaultBlockState()
+                                    //.setValue(SoulVineBlock.getPropertyForFace(Direction.UP), Boolean.valueOf(true))
+                                    .setValue(SoulVineBlock.getPropertyForFace(Direction.UP), Boolean.valueOf(true))
+                            ,
+                            2);
+    }
+}
