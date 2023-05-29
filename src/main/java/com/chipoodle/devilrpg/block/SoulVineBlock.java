09bc2948427477ad2909b0e7401c1462db6938cf
Chipoodle <ccsh23@yahoo.com.mx>
2023-05-29 17:55:30 -0600

    Updated to 1.19.3. Impemented summonig of vine algorithm

--- src/main/java/com/chipoodle/devilrpg/block/SoulVineBlock.java
+++ src/main/java/com/chipoodle/devilrpg/block/SoulVineBlock.java
@@ -28,6 +28,8 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class SoulVineBlock extends Block implements EntityBlock {
 
     public static final VoxelShape SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 15.0D, 12.0D);
@@ -36,7 +38,6 @@ public class SoulVineBlock extends Block implements EntityBlock {
     public static final IntegerProperty LEVEL = IntegerProperty.create("soulvine_level", 0, 30);
     private static final Integer MAX_AGE = 25;
     protected final Direction growthDirection;
-    private BlockPos currentBlockPos;
 
     public SoulVineBlock(Properties properties) {
         super(properties);
@@ -46,12 +47,7 @@ public class SoulVineBlock extends Block implements EntityBlock {
         this.registerDefaultState(this.getStateDefinition().any().setValue(LEVEL, 0));
     }
 
-    public SoulVineBlock(Properties properties, BlockPos pos) {
-        this(properties);
-        currentBlockPos = pos;
-    }
-
-    protected static @NotNull Block getBodyBlock() {
+    protected static @NotNull Block getSoulVineBlock() {
         return ModBlocks.SOUL_VINE_BLOCK.get();
     }
 
@@ -67,14 +63,87 @@ public class SoulVineBlock extends Block implements EntityBlock {
         return !p_153321_.isAir();
     }
 
-    public static boolean canStay(@NotNull BlockState blockState, @NotNull LevelReader levelReader, @NotNull BlockPos blockPos, Direction growthDirection) {
-        BlockState blockstate = levelReader.getBlockState(blockPos);
 
-        if (!canAttachTo(blockstate)) {
+    public static boolean canStay(@NotNull BlockState currentBlockState, @NotNull LevelReader levelReader, @NotNull BlockPos currentBlockPos, Direction growthDirection) {
+        BlockPos newBlockpos;
+        if (currentBlockState.getValue(AGE) == 1)
+            newBlockpos = currentBlockPos;
+        else {
+            if (!areSurroundingBlocksPlausible(levelReader, currentBlockPos))
+                return false;
+            newBlockpos = currentBlockPos.relative(growthDirection);
+        }
+        BlockState newBlockState = levelReader.getBlockState(newBlockpos);
+        return true;
+
+        /*if (!canAttachTo(newBlockState)) {
+
+            // buscar una nueva dirección donde si
+
             return false;
         } else {
-            return blockstate.is(getBodyBlock()) || blockstate.isFaceSturdy(levelReader, blockPos, growthDirection);
+            boolean faceSturdy = newBlockState.isFaceSturdy(levelReader, newBlockpos, growthDirection);
+            return faceSturdy;
+        }*/
+    }
+
+    public static Object getNextDirection(@NotNull BlockState currentBlockState, @NotNull LevelReader levelReader, @NotNull BlockPos currentBlockPos, Direction growthDirection) {
+        BlockPos newBlockpos = currentBlockPos.relative(growthDirection);
+        return null;
+
+    }
+
+    /**
+     * Verifies if at least one cube around is soulvine
+     *
+     * @param levelReader     a level reader
+     * @param currentBlockPos current block position
+     * @return wheter or not an adyascent block is soulvine
+     */
+    private static boolean areSurroundingBlocksPlausible(@NotNull LevelReader levelReader, @NotNull BlockPos currentBlockPos) {
+        Block soulVineBlock = getSoulVineBlock();
+
+
+        boolean isSoulVine = false;
+        boolean isFaceSturdy = false;
+        Direction directionFromsourceVine = null;
+        for (Direction possibleDirection : DIRECTIONS.getPossibleValues()) {
+            BlockState nextState = getNextState(levelReader, currentBlockPos, possibleDirection);
+
+            //DevilRpg.LOGGER.info("-----------------{} {}---------------",growDirection,currentBlockPos);
+            //DevilRpg.LOGGER.info("direction: {} next block: {} next position: {}",possibleDirection, nextState.getBlock(), nextPos);
+
+            if (nextState.is(soulVineBlock)) {
+                directionFromsourceVine = possibleDirection;
+                isSoulVine = true;
+            }
+
+            if (!nextState.is(soulVineBlock) && (canAttachTo(nextState))) {
+                isFaceSturdy = true;
+            }
+
+            if (isSoulVine && isFaceSturdy)
+                return true;
         }
+        if(isSoulVine){
+            Direction oppositeDirectionSourceVine = directionFromsourceVine.getOpposite();
+            Direction finalDirectionFromsourceVine = directionFromsourceVine;
+            List<Direction> directions = DIRECTIONS.getPossibleValues().stream()
+                    .filter(dir -> !dir.equals(oppositeDirectionSourceVine))
+                    .filter(dir -> !dir.equals(finalDirectionFromsourceVine))
+                    .toList();
+            for (Direction possibleDirection : directions){
+
+            }
+        }
+
+        return false;
+    }
+
+    @NotNull
+    private static BlockState getNextState(@NotNull LevelReader levelReader, @NotNull BlockPos currentBlockPos, Direction possibleDirection) {
+        BlockPos nextPos = currentBlockPos.relative(possibleDirection);
+        return levelReader.getBlockState(nextPos);
     }
 
     public @NotNull BlockState getStateForPlacement(LevelAccessor levelAccessor) {
@@ -84,7 +153,7 @@ public class SoulVineBlock extends Block implements EntityBlock {
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext p_53868_) {
         BlockState blockstate = p_53868_.getLevel().getBlockState(p_53868_.getClickedPos().relative(this.growthDirection));
-        return !blockstate.is(this.getBodyBlock()) ? this.getStateForPlacement(p_53868_.getLevel()) : this.getBodyBlock().defaultBlockState();
+        return !blockstate.is(getSoulVineBlock()) ? this.getStateForPlacement(p_53868_.getLevel()) : getSoulVineBlock().defaultBlockState();
     }
 
     public boolean isRandomlyTicking(BlockState blockState) {
@@ -95,19 +164,19 @@ public class SoulVineBlock extends Block implements EntityBlock {
         stateDefinition.add(AGE).add(DIRECTIONS).add(LEVEL);
     }
 
-    public VoxelShape getShape(@NotNull BlockState p_53880_, @NotNull BlockGetter p_53881_, @NotNull BlockPos p_53882_, @NotNull CollisionContext p_53883_) {
+    public @NotNull VoxelShape getShape(@NotNull BlockState p_53880_, @NotNull BlockGetter p_53881_, @NotNull BlockPos p_53882_, @NotNull CollisionContext p_53883_) {
         return SHAPE;
     }
 
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+    public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return ModEntityBlocks.SOUL_VINE_ENTITY_BLOCK.get().create(pos, state);
     }
 
     @Nullable
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, @NotNull BlockState blockState, @NotNull BlockEntityType<T> type) {
         return level.isClientSide ? null : (alevel, pos, aBlockstate, blockEntity) -> {
             if (blockEntity instanceof SoulVineBlockEntity soulVineBlockEntity && alevel.getGameTime() % 10 == 0) {
                 soulVineBlockEntity.tick(blockState, (ServerLevel) alevel, pos, alevel.getRandom());
