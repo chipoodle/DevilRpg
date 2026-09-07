2ab81c9c37fadbc2763b1ecb492b660c0349227a
Chipoodle <ccsh23@yahoo.com.mx>
2026-09-07 12:19:05 -0600

    SoulWolf: subir salud inicial de 8 a 11

--- src/main/java/com/chipoodle/devilrpg/entity/SoulWolf.java
+++ src/main/java/com/chipoodle/devilrpg/entity/SoulWolf.java
@@ -48,7 +48,7 @@ public class SoulWolf extends Wolf implements ITamableEntity, ISoulEntity, Power
     //private static final int NUMBER_OF_PARTICLES_ICE_ARMOR = 11;
     private static final int PROBABILITY_MULTIPLIER = 4;
     private static final int ICE_ARMOR_DURATION_TICKS = 140;
-    private static final int INITIAL_HEALTH = 8;
+    private static final int INITIAL_HEALTH = 11;
     private static final int NUMBER_OF_PARTICLES_FROST_BITE = 11;
     private int puntosAsignados = 0;
     private double saludMaxima = INITIAL_HEALTH;
